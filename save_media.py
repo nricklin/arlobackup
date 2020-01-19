499ae0ac5f8b3a91318472feaf1bf905227f17fc
Nate Ricklin <nate@rickl.in>
2020-01-19 10:15:15 -0700

    add simple lambda for storing video & thumbnails to S3 off an sqs queue

---
+++ save_media.py
@@ -0,0 +1,28 @@
+import json, boto3
+from botocore.vendored import requests
+s3 = boto3.client('s3')
+
+def handler(event, context):
+
+    print event
+    print context
+
+    message = json.loads( event['Records'][0]['body'] )
+    bucket = message['metadata']['s3bucket']
+    contentURL = message['media']['presignedContentUrl']
+    content_key = message['metadata']['content_key']
+    thumbURL = message['media']['presignedThumbnailUrl']
+    thumb_key = message['metadata']['thumbnail_key']
+
+    # get media itself
+    r = requests.get(contentURL, stream=True)
+    r.raw.decode_content = True 
+    s3.upload_fileobj(r.raw, bucket, content_key)
+
+    # get thumbnail
+    r = requests.get(thumbURL, stream=True)
+    r.raw.decode_content = True 
+    s3.upload_fileobj(r.raw, bucket, thumb_key)
+
+
+    
