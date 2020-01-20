277f484b4626ae4039c1996cb3ac047a2e7ffa00
Nate Ricklin <nate@rickl.in>
2020-01-20 09:01:36 -0700

    add snapshot_taker to build script

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ mkdir dist
 pip3 install -r requirements.txt -t ./dist
 cp save_media.py dist/save_media.py
 cp download_scheduler.py dist/download_scheduler.py
+cp snapshot_taker.py dist/snapshot_taker.py
 cd dist
 zip -r ../deploy.zip *
 cd ..
